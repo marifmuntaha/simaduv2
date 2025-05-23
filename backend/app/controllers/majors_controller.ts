85db23fd087d3b842da58caa51de3e23b1fe7ad0
server-ydh <marifmuntaha@gmail.com>
2025-05-23 12:29:30 +0700

    230520251228

--- backend/app/controllers/majors_controller.ts
+++ backend/app/controllers/majors_controller.ts
@@ -1,19 +1,87 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Major from '#models/major'
+import { storeMajorValidation, updateMajorValidation } from '#validators/major'
 
 export default class MajorsController {
   async index({ request, response }: HttpContext) {
-    let major = Major.query()
-    let majors
-    if (request.input('ladderId')) {
-      await major.where('ladderId', request.input('ladderId'))
+    try {
+      let major = Major.query()
+      let data
+      if (request.input('ladderId')) {
+        await major.where('ladderId', request.input('ladderId'))
+      }
+      if (request.input('yearId')) {
+        await major.where('yearId', request.input('yearId'))
+      }
+      if (request.input('institutionId')) {
+        await major.where('institutionId', request.input('institutionId'))
+      }
+      const majors = await major
+      if (request.input('type') === 'select') {
+        data = majors.map((item) => {
+          return { value: item.id, label: item.name }
+        })
+      } else {
+        data = majors
+      }
+      return response.status(200).json({
+        result: data,
+      })
+    } catch (error) {
+      return response.status(error.status).json(error)
     }
-    if (request.input('yearId')) {
-      await major.where('yearId', request.input('yearId'))
+  }
+
+  async store({ request, response }: HttpContext) {
+    try {
+      const data = request.body()
+      const payload = await storeMajorValidation.validate(data)
+      const major = await Major.create(payload)
+      return response.status(201).json({
+        message: 'Major created',
+        result: major,
+      })
+    } catch (error) {
+      return response.status(error.status).json(error)
     }
-    if (request.input('institutionId')) {
-      await major.where('institutionId', request.input('institutionId'))
+  }
+
+  async show({ params: { id }, response }: HttpContext) {
+    try {
+      const major = await Major.findOrFail(id)
+      return response.status(200).json({
+        result: major,
+      })
+    } catch (error) {
+      return response.status(error.status).json(error)
     }
-    console.log(major.model)
+  }
+
+  async update({ params: { id }, request, response }: HttpContext) {
+    try {
+      const data = request.body()
+      const major = await Major.findOrFail(id)
+      const payload = await updateMajorValidation.validate(data)
+      const update = await major.merge(payload).save()
+      return response.status(200).json({
+        message: 'Major updated',
+        result: update,
+      })
+    } catch (error) {
+      return response.status(error.status).json(error)
+    }
+  }
+
+  async destroy({ params: { id }, response }: HttpContext) {
+    try {
+      const major = await Major.findOrFail(id)
+      await major.delete()
+      return response.status(200).json({
+        message: 'Major deleted',
+        result: major,
+      })
+    } catch (error) {
+      return response.status(error.status).json(error)
+    }
   }
 }
--- backend/database/seeders/major_seeder.ts
+++ backend/database/seeders/major_seeder.ts
@@ -4,10 +4,38 @@ import Major from '#models/major'
 export default class extends BaseSeeder {
   async run() {
     const majors = [
-      { ladderId: 3, yearId: 5, name: 'Tahfidz', alias: 'TFZ', description: 'Unggulan Tahfidz' },
-      { ladderId: 3, yearId: 5, name: 'Kitab', alias: 'KTB', description: 'Unggulan Kitab' },
-      { ladderId: 3, yearId: 5, name: 'Sains', alias: 'SNS', description: 'Unggulan Sains' },
-      { ladderId: 3, yearId: 5, name: 'Reguler', alias: 'RGL', description: 'Program Reguler' },
+      {
+        ladderId: 3,
+        yearId: 5,
+        institutionId: 3,
+        name: 'Tahfidz',
+        alias: 'TFZ',
+        description: 'Unggulan Tahfidz',
+      },
+      {
+        ladderId: 3,
+        yearId: 5,
+        institutionId: 3,
+        name: 'Kitab',
+        alias: 'KTB',
+        description: 'Unggulan Kitab',
+      },
+      {
+        ladderId: 3,
+        yearId: 5,
+        institutionId: 3,
+        name: 'Sains',
+        alias: 'SNS',
+        description: 'Unggulan Sains',
+      },
+      {
+        ladderId: 3,
+        yearId: 5,
+        institutionId: 3,
+        name: 'Reguler',
+        alias: 'RGL',
+        description: 'Program Reguler',
+      },
     ]
 
     await Major.createMany(majors)
